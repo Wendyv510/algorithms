d3f3868bd7315603d05f5107e3e16409ba384c9a
Wendy Harris <wendyv510@hotmail.com>
2020-11-09 13:49:58 -0500

    stringIncludes js algo

---
+++ Javascript/stringIncludes.js
@@ -0,0 +1,12 @@
+function stringIncludes(word,letter){
+    let matches;
+    for(let i = 0; i< word.length; i++){
+        if(word[i]===letter){
+            matches = true 
+        }
+    }
+    return !!matches
+}
+
+stringIncludes("banana","a")
+//true 
