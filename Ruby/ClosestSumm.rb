797949f4f9b36475568af85069d0b51373a85f8d
Wendy Harris <wendyv510@hotmail.com>
2021-04-19 15:16:18 -0400

    syntax

--- Ruby/ClosestSumm.rb
+++ Ruby/ClosestSumm.rb
@@ -5,15 +5,15 @@
 
 def closest_sum(a,x)
     a.sort!               #You can choose any sorting algorithm of your choice with
-    left=min_l=0
-    right=min_r=a.length-1
+    left=min_l = 0
+    right=min_r = a.length-1
     min_sum = 1.0/0.0     #Initializing min_sum with infinity
     while(left<right)
-      sum=(a[left]+a[right])-x
+      sum = (a[left]+a[right])-x
       if sum.abs<min_sum
-          min_sum=sum.abs
-          min_l=left
-          min_r=right
+          min_sum = sum.abs
+          min_l = left
+          min_r = right
       end
       if sum<0
           left+=1
